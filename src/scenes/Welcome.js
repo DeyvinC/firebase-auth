ba9e78bf332c0b163e261a25ef8f4c63cd3d9913
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-02-16 13:56:19 -0500

    Added the login

--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function Welcome(){
+function Welcome({ user }){
+    console.log(user) //email, displayName, photoURL
     return (
         <>
-        <h1>Welcome</h1>
-        <button><Link to='/login'>Logout</Link></button>
+            <h1>Welcome</h1>
+            <h2>{user.displayName || user.email}</h2>
+            {user.photoURL && <img src={user.photoURL} 
+            alt='Profile of logged-in user' />}
+            <button><Link to='/login'>Logout</Link></button>
         </>
     )
 }
